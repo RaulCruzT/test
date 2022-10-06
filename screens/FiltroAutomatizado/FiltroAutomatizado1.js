fb58596dba1fb72d1ccbf955ba453b757072a489
RaulCruzT <cruztorresraul@gmail.com>
2022-10-06 08:41:35 -0300

    Agregada navegacion al Filtro Automatizado 1

--- screens/FiltroAutomatizado/FiltroAutomatizado1.js
+++ screens/FiltroAutomatizado/FiltroAutomatizado1.js
@@ -1,5 +1,6 @@
 import { SafeAreaView, StatusBar, ImageBackground, StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import { useNavigation } from '@react-navigation/native';
 
 // imagenes & logo
 import image from '../../assets/images/background.png';
@@ -13,17 +14,21 @@ import { useGlobalState } from '../../providers/hookstateProvider';
 
 const FiltroAutomatizado1 = () => {
     const state = useGlobalState();
+    const navigation = useNavigation();
 
     const pickImage = async () => {
       let result = await ImagePicker.launchImageLibraryAsync({
         mediaTypes: ImagePicker.MediaTypeOptions.All,
         allowsEditing: true,
-        aspect: [4, 3],
+        aspect: [1, 1],
         quality: 1,
       });
   
       if (!result.cancelled) {
         state.setImagen(result.uri);
+        navigation.navigate("Filtro Automatizado 2");
+      } else {
+        console.log("algo ha fallado!")
       }
     };
 
@@ -37,7 +42,7 @@ const FiltroAutomatizado1 = () => {
                     <Text style={styles.title}>Sube una imagen</Text>
                 </View>
                 <View style={styles.content}>
-                  <Text style={{textAlign: 'center'}}>Cuando subas una imagen de tu mascota perdida nuestro SamRobot se encargará de buscar en todas las redes sociales y nuestros datos a tu mascota</Text>
+                  <Text style={{textAlign: 'center', marginHorizontal: '6%'}}>Cuando subas una imagen de tu mascota perdida nuestro SamRobot se encargará de buscar en todas las redes sociales y nuestros datos a tu mascota</Text>
                   <TouchableOpacity style={[styles.circle, styles.shadow]} onPress={pickImage}>
                     <AntDesign name="camerao" size={100} color="#fff" />
                   </TouchableOpacity>
@@ -77,7 +82,7 @@ const styles = StyleSheet.create({
   },
   title: {
       color: '#060606',
-      fontSize: 21,
+      fontSize: 30,
       fontWeight: "500",
   },
   content: {
